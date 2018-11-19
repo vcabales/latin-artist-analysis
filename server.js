d5f6f2b3addfee760a3377b8cc207021177d8743
vcabales <vcabales96@gmail.com>
2018-11-18 22:53:07 -0600

    fixing port

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const port = 3000
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}!`))
